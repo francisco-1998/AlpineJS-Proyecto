7b724699b61247ea722d69d09a8086783482a59e
francisco-1998 <francmarinc@gmail.com>
2021-01-09 16:38:46 -0500

    Entendiendo el x-if

---
+++ js/app5.js
@@ -0,0 +1,17 @@
+function main() {
+    return{
+        open:true,
+        product: producto()
+    }
+    
+
+    function producto(){
+        const producto = {
+            nombre: 'Macbook Pro 13',
+            precio: 500,
+            activo: false
+        }
+        return producto;
+    }
+
+}
